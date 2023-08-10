cbf101c386239a40be3a646a9aaff2e2be800da2
Gra-Vi-Ty <worakan223@gmail.com>
2023-08-11 02:41:43 +0700

    upppp

---
+++ Homework/HW07/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+for( int i = start; i <= end; i++) {
+    //     for (int j = 1; j <= 9; j++) {
+    //         printf( "%d x %d = %d\n", i, j, i * j) ;
+    //     }//end for
+    //     printf( "\n" ) ;
+    // }
+    
